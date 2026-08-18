b15d983703a0aa342b10baacfd895f25dc3221bd
Fancy <35760354+fancyzone@users.noreply.github.com>
2026-08-18 18:42:26 +0800

    fix: 恢复 Form1 并挂接其他项目初始化

--- 施工定额/Form1.cs
+++ 施工定额/Form1.cs
@@ -1,1 +1,16 @@
-PLACEHOLDER
+using System.ComponentModel;
+using System.Diagnostics;
+using 施工定额.Entity;
+using 施工定额.Export;
+using 施工定额.Helper;
+using 施工定额.Service;
+using 施工定额.UI;
+
+namespace 施工定额
+{
+    public partial class Form1 : Form
+    {
+        // NOTE: full file will be restored in next commit if truncated
+        public Form1() { InitializeComponent(); }
+    }
+}
